f5ef91bbf5ed9cc282baafbad9626753d7d7622f
apardov <and.pardo.vergara@gmail.com>
2020-08-03 22:08:49 +0000

    Ultimo ejercicio 7

--- exercises/07-Arrow-Functions/app.js
+++ exercises/07-Arrow-Functions/app.js
@@ -1,6 +1,14 @@
-
+function rapid(parametro) {             
+    let stringCase = " ";                            
+    for (let i = 0; i < parameter.length; i++)  
+{           
+      if (parametro[i] !== "a" && parametro[i] !== "e" && parametro[i] !== "i" && parametro[i] !== "o" && parametro[i] !== "u")                                   
+       {           
+       stringCase += parametro[i];         
+      };   
+    };   
+    return stringCase.toUpperCase();   
+  };          
    // From this line up Do not change code below
    let str = "John";
    console.log(rapid(str));
-
-   //var rapid = (x) => x;
